2511755aa644fdd669f7ba04a6af6fb91da038b8
EliteMasterEric <ericmyllyoja@gmail.com>
2024-08-22 08:55:11 -0400

    Data generator fixes

--- obsidianarmor/src/com/elitemastereric/obsidianarmor/ObsidianArmorDataGenerator.hx
+++ obsidianarmor/src/com/elitemastereric/obsidianarmor/ObsidianArmorDataGenerator.hx
@@ -3,13 +3,15 @@ package com.elitemastereric.obsidianarmor;
 import net.minecraft.tags.ItemTags;
 import com.elitemastereric.obsidianarmor.items.ModItems;
 import com.elitemastereric.obsidianarmor.ObsidianArmorMod;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import java.nio.file.Path;
 import net.minecraft.core.HolderLookup.HolderLookup_Provider;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider.FabricLanguageProvider_TranslationBuilder as TranslationBuilder;
-import net.minecraft.data.models.ItemModelGenerators;
+import net.pickhaxe.compat.data.models.ItemModelGenerators;
 import net.minecraft.data.models.model.ModelTemplates;
 import net.pickhaxe.datagen.DataGenerator;
+#if fabric
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider.FabricLanguageProvider_TranslationBuilder as TranslationBuilder;
+#end
 
 /**
  * Provides an interface through which JSON files for the resources folder can be generated through code.
@@ -18,21 +20,21 @@ import net.pickhaxe.datagen.DataGenerator;
  */
 class ObsidianArmorDataGenerator extends DataGenerator {
 
-	public override function onGenerateItemTags(itemTagProvider:PickHaxeItemTagProvider, provider:HolderLookup_Provider):Void {
+	public override function onGenerateItemTags(itemTagProvider:PickHaxeItemTagsProvider, provider:HolderLookup_Provider):Void {
 		ObsidianArmorMod.LOGGER.info("Generating Obsidian Armor item tags...");
 
 		// Apply tool tags to tools.
-		itemTagProvider.getTagBuilder(ItemTags.SWORDS).add(ModItems.OBSIDIAN_SWORD);
-        itemTagProvider.getTagBuilder(ItemTags.PICKAXES).add(ModItems.OBSIDIAN_PICKAXE);
-        itemTagProvider.getTagBuilder(ItemTags.SHOVELS).add(ModItems.OBSIDIAN_SHOVEL);
-        itemTagProvider.getTagBuilder(ItemTags.AXES).add(ModItems.OBSIDIAN_AXE);
-        itemTagProvider.getTagBuilder(ItemTags.HOES).add(ModItems.OBSIDIAN_HOE);
+		itemTagProvider.getTagBuilder(ItemTags.SWORDS).add(ModItems.OBSIDIAN_SWORD.getKey());
+        itemTagProvider.getTagBuilder(ItemTags.PICKAXES).add(ModItems.OBSIDIAN_PICKAXE.getKey());
+        itemTagProvider.getTagBuilder(ItemTags.SHOVELS).add(ModItems.OBSIDIAN_SHOVEL.getKey());
+        itemTagProvider.getTagBuilder(ItemTags.AXES).add(ModItems.OBSIDIAN_AXE.getKey());
+        itemTagProvider.getTagBuilder(ItemTags.HOES).add(ModItems.OBSIDIAN_HOE.getKey());
 
         // Apply armor tags to armor.
-        itemTagProvider.getTagBuilder(ItemTags.HEAD_ARMOR).add(ModItems.OBSIDIAN_HELMET);
-        itemTagProvider.getTagBuilder(ItemTags.CHEST_ARMOR).add(ModItems.OBSIDIAN_CHESTPLATE);
-        itemTagProvider.getTagBuilder(ItemTags.LEG_ARMOR).add(ModItems.OBSIDIAN_LEGGINGS);
-        itemTagProvider.getTagBuilder(ItemTags.FOOT_ARMOR).add(ModItems.OBSIDIAN_BOOTS);
+        itemTagProvider.getTagBuilder(ItemTags.HEAD_ARMOR).add(ModItems.OBSIDIAN_HELMET.getKey());
+        itemTagProvider.getTagBuilder(ItemTags.CHEST_ARMOR).add(ModItems.OBSIDIAN_CHESTPLATE.getKey());
+        itemTagProvider.getTagBuilder(ItemTags.LEG_ARMOR).add(ModItems.OBSIDIAN_LEGGINGS.getKey());
+        itemTagProvider.getTagBuilder(ItemTags.FOOT_ARMOR).add(ModItems.OBSIDIAN_BOOTS.getKey());
 	}
 
     public override function onGenerateItemModels(itemModelGenerators:ItemModelGenerators):Void {
@@ -52,6 +54,7 @@ class ObsidianArmorDataGenerator extends DataGenerator {
         itemModelGenerators.generateArmorTrims(cast ModItems.OBSIDIAN_BOOTS);
 	}
 
+	#if fabric
     public override function onGenerateTranslations(dataOutput:FabricDataOutput, languageCode:String, translationBuilder:TranslationBuilder):Void {
 		switch (languageCode) {
 			case "en_us":
@@ -77,4 +80,5 @@ class ObsidianArmorDataGenerator extends DataGenerator {
 		// Hardcoded translations.
 		addBaseTranslations(dataOutput, 'en_us', translationBuilder);
     }
+	#end
 }
